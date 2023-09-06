2e2642a80fb8cf25bfe07e98bd90560c856f3117
priya-km <priya.k.mah@gmail.com>
2023-09-05 20:19:52 -0400

    Added css

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-navigation',
@@ -7,8 +8,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent {
-
-  constructor(private router: Router) { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public router: Router
+  ) { }
 
   ngOnInit(): void { }
 
